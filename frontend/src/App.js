3fb55a54c1446408a8ddf881552ee5ac216aff28
mastersancho <1maltsevalex@gmail.com>
2020-12-13 09:29:34 +0200

    001-Front End
    => React Setup
    => React-Bootstrap Setup
    => Header & Footer Components

---
+++ frontend/src/App.js
@@ -0,0 +1,19 @@
+import { Container } from 'react-bootstrap';
+import Footer from './components/Footer';
+import Header from './components/Header';
+
+const App = () => {
+    return (
+        <>
+            <Header />
+            <main className='py-3'>
+                <Container>
+                    <h1>Hello World</h1>
+                </Container>
+            </main>
+            <Footer />
+        </>
+    );
+};
+
+export default App;
