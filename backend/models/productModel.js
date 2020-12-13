39c2401eff85c280257e0ca6ddb6a835b693e811
mastersancho <1maltsevalex@gmail.com>
2020-12-13 15:26:06 +0200

    014-MongoDB
    => Data Seeder Script

--- backend/models/productModel.js
+++ backend/models/productModel.js
@@ -22,7 +22,7 @@ const productSchema = mongoose.Schema(
             type: String,
             required: true,
         },
-        images: {
+        image: {
             type: String,
             required: true,
         },
