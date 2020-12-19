e58ab6d38391fcc4f7aa29f5acdcbdf068a87b31
Alex M <@MasterSancho>
2020-12-19 17:38:02 +0200

    070-Admin Screens 2
    => Admin Order List

--- frontend/src/reducers/orderReducers.js
+++ frontend/src/reducers/orderReducers.js
@@ -13,6 +13,9 @@ import {
  ORDER_LIST_MY_SUCCESS,
  ORDER_LIST_MY_FAIL,
  ORDER_LIST_MY_RESET,
+ ORDER_LIST_REQUEST,
+ ORDER_LIST_SUCCESS,
+ ORDER_LIST_FAIL,
 } from '../constants/orderConstants';
 
 export const orderCreateReducer = (state = {}, action) => {
@@ -88,23 +91,26 @@ export const orderPayReducer = (state = {}, action) => {
 export const orderListMyReducer = (state = { orders: [] }, action) => {
  switch (action.type) {
   case ORDER_LIST_MY_REQUEST:
-   return {
-    loading: true,
-   };
+   return { loading: true };
   case ORDER_LIST_MY_SUCCESS:
-   return {
-    loading: false,
-    orders: action.payload,
-   };
+   return { loading: false, orders: action.payload };
   case ORDER_LIST_MY_FAIL:
-   return {
-    loading: false,
-    error: action.payload,
-   };
+   return { loading: false, error: action.payload };
   case ORDER_LIST_MY_RESET:
-   return {
-    orders: [],
-   };
+   return { orders: [] };
+  default:
+   return state;
+ }
+};
+
+export const orderListReducer = (state = { orders: [] }, action) => {
+ switch (action.type) {
+  case ORDER_LIST_REQUEST:
+   return { loading: true };
+  case ORDER_LIST_SUCCESS:
+   return { loading: false, orders: action.payload };
+  case ORDER_LIST_FAIL:
+   return { loading: false, error: action.payload };
   default:
    return state;
  }
